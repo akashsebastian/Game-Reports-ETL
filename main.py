b339c306ed26eb516c8842f5a4306b42cc06006e
akash <akash.sebastian6@gmail.com>
2021-04-07 15:09:28 -0500

    Added daily poller

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ from scores import Scores
 
 def back_populator():
     # Start date of season
-    # start_date = datetime.date(2020, 12, 22)
-    start_date = datetime.date(2021, 4, 2)
+    start_date = datetime.date(2020, 12, 22)
     end_date, _ = get_dates()
     while start_date <= end_date:
         Report().generate_reports(start_date)
         Scores(start_date).poll()
         start_date = start_date + datetime.timedelta(days=1)
 
+def poll():
+    _, yesterday = get_dates()
+    print(f"Polling for date: {yesterday}")
+    Report().generate_reports(yesterday)
+    Scores(yesterday).poll()
+
 if __name__ == '__main__':
-    back_populator()
+    poll()
